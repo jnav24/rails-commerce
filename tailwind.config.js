896aec78d1f589a1d4b0b3724df1aa393eca3e6a
Justin Navarro <info@justinnavarro.net>
2024-05-22 06:37:05 -0400

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.orange["400"],
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
